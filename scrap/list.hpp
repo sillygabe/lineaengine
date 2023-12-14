af9e6dc54acc48fb6139b41d68b3a777a57920d0
sillygabe <gabrielonline630@gmail.com>
2023-12-14 19:35:39 +0300

    Added the list (dynamic array)

--- scrap/list.hpp
+++ scrap/list.hpp
@@ -1,10 +1,47 @@
-#ifndef _GLIBCXX_CSTDLIB
 #include <cstdlib>
-#endif
 
 template <typename T>
 class list
 {
-    //TODO
-    //Dynamic array 
+private:
+    size_t size;
+    size_t max_size;
+    T* arr;
+public:
+    list()
+    {
+        this->arr = (T*) malloc(0);
+        this->size = 0;
+    }
+
+    void append(T elem)
+    {
+        this->arr = (T*) realloc(this->arr, ++this->size);
+        this->arr[this->size - 1] = elem;
+    }   
+
+    void pop()
+    {
+        this->arr = (T*) realloc(this->arr, --this->size);
+    }
+
+    T *operator[](const size_t pos)
+    {
+        return &(this->arr[pos]);
+    }
+
+    T *begin()
+    {
+        return &(this->arr[0]);
+    }
+
+    T *end()
+    {
+        return &(this->arr[size]);
+    }
+
+    size_t get_size()
+    {
+        return this->size;
+    }
 };
