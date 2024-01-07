0d21a66da2a9ab137790aff61403fcb03341e25c
sillygabe <gabrielonline630@gmail.com>
2024-01-07 12:10:25 +0300

    Added is_same to define.hpp

--- base/define.hpp
+++ base/define.hpp
@@ -1,5 +1,26 @@
+#include <ctype.h>
 #pragma once
 
-#include <ctype.h>
 typedef float flt32_t;
 typedef double flt64_t;
+
+//struct to check if to types are the same :)
+template <typename T, typename U>
+struct is_same
+{
+    static constexpr bool value = false;
+    constexpr operator bool() const 
+    {
+        return false;
+    }
+};
+
+template <typename T>
+struct is_same<T, T>
+{
+    static constexpr bool value = true;
+    constexpr operator bool() const 
+    {
+        return true;
+    }
+};
